32f1ce16ce9df1f4615a0403ed56bf6fd7dbbef4
obsproth <obsproth@users.noreply.github.com>
2017-03-19 11:58:06 +0900

    Add missing return of Event methods

--- slackbotpry/event.py
+++ slackbotpry/event.py
@@ -5,16 +5,16 @@ class Event:
     def post_message(self, text, channel=None):
         if channel is None:
             channel = self.data['channel']
-        self.bot.post_message(text, channel)
+        return self.bot.post_message(text, channel)
     def add_reaction(self, emoji, channel=None, timestamp=None):
         if channel is None:
             channel = self.data['channel']
         if timestamp is None:
             timestamp = self.data['ts']
-        self.bot.add_reaction(emoji, channel, timestamp)
+        return self.bot.add_reaction(emoji, channel, timestamp)
     def remove_reaction(self, emoji, channel=None, timestamp=None):
         if channel is None:
             channel = self.data['channel']
         if timestamp is None:
             timestamp = self.data['ts']
-        self.bot.remove_reaction(emoji, channel, timestamp)
+        return self.bot.remove_reaction(emoji, channel, timestamp)
