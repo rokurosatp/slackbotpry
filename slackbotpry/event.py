b85250b33d7bf78f2e2e16e28f40343a28018938
obsproth <obsproth@users.noreply.github.com>
2017-03-23 17:17:53 +0900

    Removed unused args in Event methods

--- slackbotpry/event.py
+++ slackbotpry/event.py
@@ -2,19 +2,14 @@ class Event:
     def __init__(self, bot, data):
         self.bot = bot
         self.data = data
-    def post_message(self, text, channel=None):
-        if channel is None:
-            channel = self.data['channel']
+    def post_message(self, text):
+        channel = self.data['channel']
         return self.bot.post_message(text, channel)
-    def add_reaction(self, emoji, channel=None, timestamp=None):
-        if channel is None:
-            channel = self.data['channel']
-        if timestamp is None:
-            timestamp = self.data['ts']
+    def add_reaction(self, emoji):
+        channel = self.data['channel']
+        timestamp = self.data['ts']
         return self.bot.add_reaction(emoji, channel, timestamp)
-    def remove_reaction(self, emoji, channel=None, timestamp=None):
-        if channel is None:
-            channel = self.data['channel']
-        if timestamp is None:
-            timestamp = self.data['ts']
+    def remove_reaction(self, emoji):
+        channel = self.data['channel']
+        timestamp = self.data['ts']
         return self.bot.remove_reaction(emoji, channel, timestamp)
