e9f3a54005fa714b7e7dcc86491b1ddabfbd9003
obsproth <obsproth@users.noreply.github.com>
2017-07-11 12:15:05 +0900

    Add "except Exception" into mainloop for stability

--- slackbotpry/bot.py
+++ slackbotpry/bot.py
@@ -56,6 +56,10 @@ class Bot:
             except KeyboardInterrupt:
                 print('shutting down')
                 break
+            except Exception as e:
+                print(e.args)
+                sleep(30)
+                continue
 #        for handler in self.handlers:
 #            handler.event_queue.join()
         sleep(3)
